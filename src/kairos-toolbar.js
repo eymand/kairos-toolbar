590801557a0c2a6d9a3bfed2b731a0146331d5b1
Karl Stolley <karl.stolley@gmail.com>
2015-12-31 15:22:17 -0600

    Bumped copyright date to 2016 and added MIT License

--- src/kairos-toolbar.js
+++ src/kairos-toolbar.js
@@ -5,15 +5,39 @@
  * Created by Karl Stolley
  * http://karlstolley.com/ - @karlstolley
  *
- * Copyright 2013 Kairos: A Journal of Rhetoric, Technology,
- * and Pedagogy.
- * http://kairos.technorhetoric.net
- *
  * A small jQuery plugin that takes Dublin Core metadata and
  * a few other bits of data and makes citation information
  * available, along with Kairos branding, on webtexts
  * published by Kairos.
  * https://github.com/karlstolley/kairos-toolbar
+ *
+ *
+ *
+ * The MIT License (MIT) Copyright (c) 2016 Kairos: A Journal
+ * of Rhetoric, Technology, and Pedagogy.
+ * http://kairos.technorhetoric.net
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall
+ * be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * ========================================================== */
 
